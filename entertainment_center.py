67672daa19181942bbb2a3fb30af4b6bb630fc67
Laurie Cuffney <Laurie Cuffney>
2016-06-06 19:06:45 -0400

    initial commit

---
+++ entertainment_center.py
@@ -0,0 +1,12 @@
+import media
+
+    the_beauty_inside = media.Movie("The Beauty Inside",
+        "All relationship have issues, but Woo-jin's identity crisis is a new"
+        " one-in fact, it's new one every day. Every morning, Woo-jin wakes up "
+        "in a different body. His age, gender, and nationality many change, but"
+        " the one constant in his life is Yi-Soo- the woman he loves. She knows "
+        "his secret, and loves him anyway. With every transformation, Woo-jin "
+        "has to figure out how to return to his own body and reunite with "
+        "Yi-soo.", # movie description pulled from imdb
+        "http://www.gstatic.com/tv/thumb/movieposters/12066602/p12066602_p_v8_aa.jpg",
+        "https://www.youtube.com/watch?v=PnTgpbfqcdU")
